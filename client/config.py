a58184f100dbce68c9398c0868d450f431380cde
Blunderbuss01 <110673734+Blunderbuss01@users.noreply.github.com>
2022-08-14 19:16:05 +0100

    config.py

---
+++ client/config.py
@@ -0,0 +1,73 @@
+from getpass import getpass
+
+while True:
+    print("Enter a numeric character to configure its associated setting:")
+    print("1 - regular FTP pull interval")
+    print("2 - server hostname")
+    print("3 - server port")
+    print("4 - server username")
+    print("5 - server password")
+    print("(6 - exit program)\n")
+
+    #validate input
+    try:
+        op = int(input(""))
+    except ValueError:
+        print("Invalid input.\n")
+        continue
+    if not (1<=op and op<=6):
+        print("Invalid input.\n")
+        continue
+
+    #record config file in current state
+    with open("config.txt", 'a+') as file:
+        file.seek(0)
+        lines = file.readlines()
+        linesNo = len(lines)
+        #if latter fields are blank, add newlines to avoid index error
+        if (linesNo<6):
+            for i in range (5-linesNo):
+                lines.append('\n')
+            lines.append('')
+
+    #(exit program)
+    if (op == 6):
+        break
+
+    var=''
+
+    
+    #(interval)
+    if (op == 1):
+        var = input("Enter new interval (in seconds), or press enter to cancel: ")
+    #(host)
+    if (op == 2):
+        var = input("Enter new server host, or press enter to cancel: ")
+    #(port)
+    if (op == 3):
+        var = input("Enter new server port, or press enter to cancel: ")
+    #(user)
+    if (op == 4):
+        var = input("Enter new username, or press enter to cancel: ")
+
+    #op corresponds to line of file to be changed  
+    lines[op] = (var + '\n')
+    #(line 0 of file is reserved for recording 'last' variable (last pull from server))      
+    
+    #(pswd)
+    if (op == 5):
+        #getpass doesn't work in idle, but would hide the user's input on the terminal
+        var = getpass("Enter new password, or press enter to cancel: ")
+        lines[op] = (var)
+
+    #if enter key was pressed, cancel
+    if (var == ''):
+        print("File write cancelled.\n")
+        continue
+
+    #write to file with user's alteration
+    with open('config.txt', 'w+') as file:
+        for line in lines:
+            file.write(line)
+
+    print('')
